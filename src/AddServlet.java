7116eb4174fea8fc1db91b1449dc23eb97e216ef
Amit Gupta <iamitgupta1994@gmail.com>
2020-04-01 15:25:35 +0545

    [add][dev]: add two number in AddServlet via request from Add form

--- src/AddServlet.java
+++ src/AddServlet.java
@@ -1,5 +1,24 @@
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class AddServlet extends HttpServlet {
 
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("This is AddServlet ");
+
+        int num1 = Integer.parseInt(req.getParameter("num1"));
+        int num2 = Integer.parseInt(req.getParameter("num2"));
+
+        int sum = num1 + num2;
+
+        PrintWriter out = resp.getWriter();
+        out.println("result is:" + sum);
+
+    }
 }
--- src/FormServlet.java
+++ src/FormServlet.java
@@ -10,11 +10,6 @@ public class FormServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-//        int num1 = Integer.parseInt(req.getParameter("num1"));
-//        int num2 = Integer.parseInt(req.getParameter("num2"));
-//
-//        int sum = num1 + num2;
         System.out.println("This is FormServlet ");
 
 //        PrintWriter out = resp.getWriter();
