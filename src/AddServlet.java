ee16a1234673dcd4e328323371701592bc004cb7
Amit Gupta <iamitgupta1994@gmail.com>
2020-04-02 11:16:28 +0545

    [add][dev]: use of Servlet Annotaion Configuration instead of web.xml for routing requests

--- src/AddServlet.java
+++ src/AddServlet.java
@@ -1,10 +1,12 @@
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet("/add")
 public class AddServlet extends HttpServlet {
 
 
